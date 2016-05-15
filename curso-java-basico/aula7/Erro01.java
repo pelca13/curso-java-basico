f61b529744bf63b2ff1232e251f85a57c14ff923
pelca13 <pelca13@gmail.com>
2016-05-14 23:26:27 -0300

    Add files via upload

--- curso-java-basico/aula7/Erro01.java
+++ curso-java-basico/aula7/Erro01.java
@@ -1,5 +1,7 @@
 class Erro01 {
-	public static void main(String[] args){
-		System.out.println("Hello Word");
+	
+	public static void main (String[] args){
+
+		System.out.println("Hello World");
 	}
 }
--- curso-java-basico/aula7/Erro03.java
+++ curso-java-basico/aula7/Erro03.java
@@ -1,5 +1,8 @@
 class Erro03 {
-	public static void main(String[] args){
-		System.out.println("Hello Word");
+	
+	public static void main (String[] args){
+
+		//System.out.println("Voce digitou: ");
+		System.out.println("Voce digitou: " + 1/0);
 	}
 }
